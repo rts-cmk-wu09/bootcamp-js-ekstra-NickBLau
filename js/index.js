8c268a24fa1224d2aff2f3be579c45a76281ebfa
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-03 11:19:04 +0100

    add random.person to javascript.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
+let person = document.querySelector("#aktuelle-person-navn")
+
 
 document.addEventListener ("DOMContentLoaded", (event) => {
 
+let myArray = [
+		"Fedtmule",
+		"Anders",
+		"Mickey",
+]
+console.log(myArray);
+
+
+let random = Math.floor(Math.random() * myArray.length)
+
+console.log(random);
+person.innerText = myArray [random]
 	// Slå dig løs her
 
 }); // Afslutter: DOMContentLoaded
