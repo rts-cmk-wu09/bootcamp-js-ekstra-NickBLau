42557d55a877eb8962171e87c4b411c73ba286da
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-03 12:01:50 +0100

    add myAgeArray

--- js/index.js
+++ js/index.js
@@ -1,19 +1,28 @@
 let person = document.querySelector("#aktuelle-person-navn")
-
+let alder = document.querySelector("#aktuelle-person-alder")
 
 document.addEventListener ("DOMContentLoaded", (event) => {
 
-let myArray = [
+let myPersonArray = [
 		"Fedtmule",
 		"Anders",
 		"Mickey",
 ]
 
+let myAgeArray = [
+	"60år",
+	"73år",
+	"68år",
+]
 
-let random = Math.floor(Math.random() * myArray.length)
+
+let random = Math.floor(Math.random() * myPersonArray.length)
 
 
-person.innerText = myArray [random]
+
+person.innerText = myPersonArray [random]
+alder.innerText = myAgeArray [random]
 	// Slå dig løs her
+
 
 }); // Afslutter: DOMContentLoaded
